e02c52155eb2b45fdacf656201a205e656f491cc
Fahrul Ahyan <80196937+pod-III@users.noreply.github.com>
2026-04-22 16:24:22 +0700

    edit supabase reroute

--- supabase.js
+++ supabase.js
@@ -151,7 +151,7 @@ async function sendPasswordReset(email) {
   if (isSandbox()) return { error: "Reset not available in Sandbox." };
   
   // Update this URL to where your actual reset page is hosted
-  const resetUrl = window.location.origin + 'api/reset-password/index.html';
+  const resetUrl = window.location.origin + 'api/reset-password.html';
   
   return await db.auth.resetPasswordForEmail(email, {
     redirectTo: resetUrl,
