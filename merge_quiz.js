80b5f4d0e8db799ccb82459e57cc170ab7b94594
Fahrul Ahyan <fahyandp@gmail.com>
2026-05-14 23:48:08 +0700

    chore: remove obsolete build utility scripts and configuration file

--- merge_quiz.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const oldQuizHtml = fs.readFileSync('apps/games/quiz/index.html', 'utf8');
-const wipIndexHtml = fs.readFileSync('apps/wip/quiz/index.html', 'utf8');
-const wipEditorHtml = fs.readFileSync('apps/wip/quiz/editor.html', 'utf8');
-
-// I'll manually construct the merged HTML file here in the next step,
-// but first let's see if there are any specific assets.
-console.log('Read all files');
